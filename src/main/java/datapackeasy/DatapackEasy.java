5e75235168d609498cbab8718e8ae38d7bc3879a
Deximus-Maximus <kev12east@gmail.com>
2023-11-13 17:15:11 -0500

    Fix datapacks being copied to all worlds Fix not functioning on dedicated servers Closes GH-1

--- src/main/java/datapackeasy/DatapackEasy.java
+++ src/main/java/datapackeasy/DatapackEasy.java
@@ -1,8 +1,9 @@
 package datapackeasy;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.server.ServerStartCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.WorldSavePath;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,90 +19,65 @@ public class DatapackEasy implements ModInitializer {
 	public void onInitialize() {
 
 		//Get main directory
-		Path mainDir = FabricLoader.getInstance().getGameDirectory().toPath();
+		Path mainDir = FabricLoader.getInstance().getGameDir();
 
 		Path datapackSource = mainDir.resolve("Global Datapacks");
 		if (!datapackSource.toFile().exists()) {
 			datapackSource.toFile().mkdir();
 		}
 
-		ServerStartCallback.EVENT.register(t -> {
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
 			//Variable to check if datapacks changed
 			boolean changed = false;
 
 			//get datapacks from master folder
 			List<Path> globalDatapacks = null;
-			try {
-				globalDatapacks = Files.walk(datapackSource, 1)
-						.collect(Collectors.toList());
+			try (var walk = Files.walk(datapackSource, 1)) {
+				globalDatapacks = walk.collect(Collectors.toList());
 				globalDatapacks.remove(0);
 			} catch (IOException e) {
 				System.out.println("Error finding global datapacks!!");
 				e.printStackTrace();
 			}
 
-			//Save directory
-			Path saveDir = mainDir.resolve("saves");
+			Path worldDatapackDir = server.getSavePath(WorldSavePath.DATAPACKS);
 
-			//get world folders
-			List<Path> worldfolders = null;
-			try {
-				worldfolders = Files.walk(saveDir, 1)
-						.filter(Files::isDirectory)
-						.collect(Collectors.toList());
-				worldfolders.remove(saveDir);
+			List<Path> datapacks = null;
+			try (var datapackStream = Files.walk(worldDatapackDir, 1)) {
+				datapacks = datapackStream.collect(Collectors.toList());
+				datapacks.remove(worldDatapackDir);
 			} catch (IOException e) {
-				System.out.println("Error finding world folders!");
 				e.printStackTrace();
 			}
 
-			//get datapack folders
-			if (worldfolders != null) {
-				for (Path worldFolder : worldfolders) {
-					Path worldDatapackDir = worldFolder.resolve("datapacks");
-					//for (Path wldDp : worldDatapackDir) {
-						List<Path> datapacks = null;
+			//copy datapacks over
+			if (globalDatapacks != null) {
+				for (Path datapack : globalDatapacks) {
+					//keep track of datapack in the world folder or not
+					boolean alreadyExists = false;
+					if (datapacks != null) {
+						for (Path localDatapack : datapacks) {
+							alreadyExists = datapack.getFileName().equals(localDatapack.getFileName());
+						}
+					}
+					if (!alreadyExists) {
 						try {
-							datapacks = Files.walk(worldDatapackDir, 1)
-									.collect(Collectors.toList());
-							datapacks.remove(worldDatapackDir);
+							Files.copy(datapack, new FileOutputStream(Paths.get(worldDatapackDir.toString(), datapack.toFile().getName()).toString()));
+							changed = true;
+							System.out.println("Added datapacks to world");
 						} catch (IOException e) {
+							System.out.println("Error copying datapacks to world folder!");
 							e.printStackTrace();
 						}
-
-						//copy datapacks over
-						if (globalDatapacks != null) {
-							for (Path datapack : globalDatapacks) {
-								//keep track of datapack in the world folder or not
-								boolean alreadyExists = false;
-								if (datapacks != null) {
-									for (Path localDatapack : datapacks) {
-										alreadyExists = datapack.getFileName().equals(localDatapack.getFileName());
-									}
-								}
-								if (!alreadyExists) {
-									try {
-										Files.copy(datapack, new FileOutputStream(Paths.get(worldDatapackDir.toString(), datapack.toFile().getName()).toString()));
-										changed = true;
-										System.out.println("Added datapacks to world");
-									} catch (IOException e) {
-										System.out.println("Error copying datapacks to world folder!");
-										e.printStackTrace();
-									}
-								}
-							}
-						}
-
 					}
-				//}
+				}
 			}
 
 			//Reload the datapacks if needed
 			if (changed) {
+				server.getCommandManager().executeWithPrefix(server.getCommandSource(), "/reload");
 				System.out.println("Reloaded datapacks");
-				t.reload();
 			}
-
 		});
 
 	}
